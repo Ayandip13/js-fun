276b82673d4d52c9a03f15280bbb0e042e0e2579
ayandip paul <ayandippaul284@gmail.com>
2024-11-15 13:03:20 +0530

    add js and some style

--- one.js
+++ one.js
@@ -13,22 +13,22 @@ let addTask = () => {
 
 let updateTasksList = () => {
     let tasksList = document.getElementById('task-list');
-    tasksList.innerHTML = ' ';
+    tasksList.innerHTML = 'no';
     tasks.forEach((task, index) => {
         const listitem = document.createElement('li')
 
         listitem.innerHTML = `
         <div class='taskItem'>
-            <div class='task ${task}'>
-                <input type='checkbox' class='checkbox'/>
-                <p>Finish this project</p>
+            <div class='task ${task.completed? 'completed':""}'>               
+                <input type='checkbox' class='checkbox' ${task.completed? 'checked' : ""}/>
+                <p>${task.text}</p>
             </div>
             <div>
-                <i class="fa-regular fa-pen-to-square"></i>
-                <i class="fa-solid fa-trash"></i>
+                <i class="fa-regular fa-pen-to-square" onClick="editTask(${index})"></i>
+                <i class="fa-solid fa-trash" onclick="deletTask(${index})"></i>
             </div>
         </div>
-        `
+        `  //did not understand the specific line inside string interpolations...
 
         console.log(index);
         
@@ -40,7 +40,7 @@ let updateTasksList = () => {
 }
 
 
-document.getElementById("newTask").addEventListener("click", function (e) {
+document.getElementById("newTask").addEventListener("click", function (e) { 
   e.preventDefault();
 
   addTask();
